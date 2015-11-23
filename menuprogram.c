7a4682c3cda7c2795ba73abc3f1b1820c1e1d9df
Dillon Mabry <dcmabry@gmail.com>
2015-11-23 10:13:26 -0500

    Deleted unused includes

--- menuprogram.c
+++ menuprogram.c
@@ -3,10 +3,8 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <dirent.h> 
-#include <stdio.h> 
 #include <stdlib.h>
 #include <string.h>
-#include <dirent.h>
 #include <err.h>
 #include <sys/stat.h>
 #include <signal.h>
